6f8b3f68b1e1938f67d272169792279bd2736b65
nndaanda <104778823+nndaanda@users.noreply.github.com>
2022-07-21 01:02:16 +0900

    Update __main__.py

--- ProjectMan/__main__.py
+++ ProjectMan/__main__.py
@@ -5,7 +5,7 @@
 # PLease read the GNU Affero General Public License in
 # <https://www.github.com/mrismanaziz/PyroMan-Userbot/blob/main/LICENSE/>.
 #
-# t.me/SharingUserbot & t.me/Lunatic0de
+# t.me/gemoyysupport & @gemoyyproject
 
 from pyrogram import idle
 
